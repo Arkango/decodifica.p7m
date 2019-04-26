413016acb1538e42e76ffa1624900b874a2a9e14
Arcangelo Saracino <saracinoarcangelo@gmail.com>
2019-04-26 10:52:38 +0200

    file con funzione di decodifica

---
+++ decodificaFattura.php
@@ -0,0 +1,27 @@
+<?php
+define("PATH_XML_EXTRACT",'./');
+
+
+
+function decodificaFattura($path){
+
+	if(file_exists($path) && (strpos($path, ".p7m") !== false)){
+		//openssl smime -verify -inform DER -in IT01879020517_g00b5.xml.p7m  -noverify -out file_finale
+		$filename_output = time().".xml";
+
+		$command = "openssl smime -verify -inform DER -in ".$path."  -noverify -out ".PATH_XML_EXTRACT.$filename_output;
+		exec( $command, $output, $return_var );
+
+
+		
+		if(!empty($output)){ return $output; }
+		if(!empty($return_var)){ return $retun_var;}
+		
+		return $filename_output;
+
+	}else{ return -1;}
+
+}
+
+print_r( decodificaFattura('Downloads/IT01879020517_g00b5.xml.p7m'));
+ 
